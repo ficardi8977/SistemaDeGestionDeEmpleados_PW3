cb2142da50dc1c35f228790bd6f4fd7e52eb5754
Fernando Icardi <fernando.icardi@gmail.com>
2024-06-11 00:37:04 -0300

    agregado validaciones dataanotation

--- SistemaDeGestionDeEmpleados.Web/Models/EmpleadoRequestViewModel.cs
+++ SistemaDeGestionDeEmpleados.Web/Models/EmpleadoRequestViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace SistemaDeGestionDeEmpleados.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaDeGestionDeEmpleados.Web.Models
 {
     public class EmpleadoRequestViewModel
     {
+        [Required(ErrorMessage = "El nombre completo es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El nombre completo no puede exceder los 100 caracteres.")]
         public string NombreCompleto { get; set; }
 
+        [Required(ErrorMessage = "La Sucursal es obligatoria.")]
         public int IdSucursal { get; set; }
 
         public EmpleadoRequestViewModel()
